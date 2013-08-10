92e24af5fb7f5c72ad79a79a979ef999477054f6
Ethan Bond <ethan@emberi.co>
2013-08-10 14:02:01 -0400

    Test

--- node/hook.js
+++ node/hook.js
@@ -5,9 +5,8 @@ gith({
         repo: 'ethanbond/bubbl.io'
 }).on('all', function(payload){
         if(payload.branch === 'master'){
-        	execFile('`whoami`');
-                // execFile('~/hooks/bubbl.io/hook.sh', function(error, stdout, stderr){
-                //         console.log('Received webhook from ethanbond/bubbl.io/master');
-                // });
+            execFile('~/hooks/bubbl.io/hook.sh', function(error, stdout, stderr){
+                    console.log('Received webhook from ethanbond/bubbl.io/master');
+            });
         }
 });
